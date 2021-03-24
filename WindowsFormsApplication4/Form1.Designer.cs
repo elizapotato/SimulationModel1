a385e06342c9cf752f7aae061713f01f498f978c
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-03-24 20:29:52 +0700

    init

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox13 = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labDay = new System.Windows.Forms.Label();
+            this.labMoney = new System.Windows.Forms.Label();
+            this.timeUp = new System.Windows.Forms.RadioButton();
+            this.timeBack = new System.Windows.Forms.RadioButton();
+            this.timeDown = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox13);
+            this.panel1.Controls.Add(this.checkBox14);
+            this.panel1.Controls.Add(this.checkBox15);
+            this.panel1.Controls.Add(this.checkBox16);
+            this.panel1.Controls.Add(this.checkBox9);
+            this.panel1.Controls.Add(this.checkBox10);
+            this.panel1.Controls.Add(this.checkBox11);
+            this.panel1.Controls.Add(this.checkBox12);
+            this.panel1.Controls.Add(this.checkBox5);
+            this.panel1.Controls.Add(this.checkBox6);
+            this.panel1.Controls.Add(this.checkBox7);
+            this.panel1.Controls.Add(this.checkBox8);
+            this.panel1.Controls.Add(this.checkBox4);
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 815);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkBox13
+            // 
+            this.checkBox13.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox13.Location = new System.Drawing.Point(669, 614);
+            this.checkBox13.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox13.Name = "checkBox13";
+            this.checkBox13.Size = new System.Drawing.Size(196, 177);
+            this.checkBox13.TabIndex = 19;
+            this.checkBox13.UseVisualStyleBackColor = true;
+            this.checkBox13.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox14.Location = new System.Drawing.Point(453, 614);
+            this.checkBox14.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(196, 177);
+            this.checkBox14.TabIndex = 18;
+            this.checkBox14.UseVisualStyleBackColor = true;
+            this.checkBox14.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox15.Location = new System.Drawing.Point(236, 614);
+            this.checkBox15.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Size = new System.Drawing.Size(196, 177);
+            this.checkBox15.TabIndex = 17;
+            this.checkBox15.UseVisualStyleBackColor = true;
+            this.checkBox15.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox16.Location = new System.Drawing.Point(20, 614);
+            this.checkBox16.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Size = new System.Drawing.Size(196, 177);
+            this.checkBox16.TabIndex = 16;
+            this.checkBox16.UseVisualStyleBackColor = true;
+            this.checkBox16.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox9.Location = new System.Drawing.Point(669, 416);
+            this.checkBox9.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(196, 177);
+            this.checkBox9.TabIndex = 15;
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox10.Location = new System.Drawing.Point(453, 416);
+            this.checkBox10.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(196, 177);
+            this.checkBox10.TabIndex = 14;
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox11.Location = new System.Drawing.Point(236, 416);
+            this.checkBox11.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(196, 177);
+            this.checkBox11.TabIndex = 13;
+            this.checkBox11.UseVisualStyleBackColor = true;
+            this.checkBox11.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox12.Location = new System.Drawing.Point(20, 416);
+            this.checkBox12.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(196, 177);
+            this.checkBox12.TabIndex = 12;
+            this.checkBox12.UseVisualStyleBackColor = true;
+            this.checkBox12.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox5.Location = new System.Drawing.Point(669, 218);
+            this.checkBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(196, 177);
+            this.checkBox5.TabIndex = 11;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox6.Location = new System.Drawing.Point(453, 218);
+            this.checkBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(196, 177);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox7.Location = new System.Drawing.Point(236, 218);
+            this.checkBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(196, 177);
+            this.checkBox7.TabIndex = 9;
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox8.Location = new System.Drawing.Point(20, 218);
+            this.checkBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(196, 177);
+            this.checkBox8.TabIndex = 8;
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox4.Location = new System.Drawing.Point(669, 21);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(196, 177);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox3.Location = new System.Drawing.Point(453, 21);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(196, 177);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox2.Location = new System.Drawing.Point(236, 21);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(196, 177);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.Location = new System.Drawing.Point(20, 21);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(196, 177);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labDay
+            // 
+            this.labDay.AutoSize = true;
+            this.labDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labDay.Location = new System.Drawing.Point(932, 36);
+            this.labDay.Name = "labDay";
+            this.labDay.Size = new System.Drawing.Size(70, 24);
+            this.labDay.TabIndex = 1;
+            this.labDay.Text = "0 день";
+            // 
+            // labMoney
+            // 
+            this.labMoney.AutoSize = true;
+            this.labMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labMoney.Location = new System.Drawing.Point(932, 66);
+            this.labMoney.Name = "labMoney";
+            this.labMoney.Size = new System.Drawing.Size(55, 24);
+            this.labMoney.TabIndex = 2;
+            this.labMoney.Text = "100 $";
+            // 
+            // timeUp
+            // 
+            this.timeUp.Appearance = System.Windows.Forms.Appearance.Button;
+            this.timeUp.BackColor = System.Drawing.Color.White;
+            this.timeUp.Location = new System.Drawing.Point(1077, 250);
+            this.timeUp.Name = "timeUp";
+            this.timeUp.Size = new System.Drawing.Size(40, 40);
+            this.timeUp.TabIndex = 5;
+            this.timeUp.TabStop = true;
+            this.timeUp.Text = ">>";
+            this.timeUp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timeUp.UseVisualStyleBackColor = false;
+            this.timeUp.CheckedChanged += new System.EventHandler(this.timeUp_CheckedChanged);
+            // 
+            // timeBack
+            // 
+            this.timeBack.Appearance = System.Windows.Forms.Appearance.Button;
+            this.timeBack.BackColor = System.Drawing.Color.DarkGray;
+            this.timeBack.Location = new System.Drawing.Point(1031, 250);
+            this.timeBack.Name = "timeBack";
+            this.timeBack.Size = new System.Drawing.Size(40, 40);
+            this.timeBack.TabIndex = 6;
+            this.timeBack.TabStop = true;
+            this.timeBack.Text = "x1";
+            this.timeBack.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timeBack.UseVisualStyleBackColor = false;
+            this.timeBack.CheckedChanged += new System.EventHandler(this.timeBack_CheckedChanged);
+            // 
+            // timeDown
+            // 
+            this.timeDown.Appearance = System.Windows.Forms.Appearance.Button;
+            this.timeDown.BackColor = System.Drawing.Color.White;
+            this.timeDown.Location = new System.Drawing.Point(985, 250);
+            this.timeDown.Name = "timeDown";
+            this.timeDown.Size = new System.Drawing.Size(40, 40);
+            this.timeDown.TabIndex = 7;
+            this.timeDown.TabStop = true;
+            this.timeDown.Text = "<<";
+            this.timeDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timeDown.UseVisualStyleBackColor = false;
+            this.timeDown.CheckedChanged += new System.EventHandler(this.timeDown_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1171, 841);
+            this.Controls.Add(this.timeDown);
+            this.Controls.Add(this.timeBack);
+            this.Controls.Add(this.timeUp);
+            this.Controls.Add(this.labMoney);
+            this.Controls.Add(this.labDay);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox checkBox13;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labDay;
+        private System.Windows.Forms.Label labMoney;
+        private System.Windows.Forms.RadioButton timeUp;
+        private System.Windows.Forms.RadioButton timeBack;
+        private System.Windows.Forms.RadioButton timeDown;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<CheckBox, Cell> field = new Dictionary<CheckBox, Cell>();
+        private int day = 0;
+        private int money = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (CheckBox cb in panel1.Controls)
+                field.Add(cb, new Cell());
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = (sender as CheckBox);
+            if (cb.Checked) Plant(cb);
+            else Harvest(cb);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (CheckBox cb in panel1.Controls)
+                NextStep(cb);
+            day++;
+            labDay.Text = day + " день";
+        }
+
+        private void Plant(CheckBox cb)
+        {
+            field[cb].Plant();
+            UpdateBox(cb);
+            money = money - 2;
+            labMoney.Text = money + " $";
+        }
+
+        private void Harvest(CheckBox cb)
+        {
+            switch (field[cb].state)
+            {
+                case CellState.Planted:
+                    break;
+                case CellState.Green:
+                    break;
+                case CellState.Immature:
+                    money = money + 3;
+                    labMoney.Text = money + " $";
+                    break;
+                case CellState.Mature:
+                    money = money + 5;
+                    labMoney.Text = money + " $";
+                    break;
+                case CellState.Overgrown:
+                    money = money - 1;
+                    labMoney.Text = money + " $";
+                    break;
+            }
+            field[cb].Harvest();
+            UpdateBox(cb);           
+        }
+
+        private void NextStep(CheckBox cb)
+        {
+            field[cb].NextStep();
+            UpdateBox(cb);
+        }
+
+        private void UpdateBox(CheckBox cb)
+        {
+            Color c = Color.White;
+            switch (field[cb].state)
+            {
+                case CellState.Planted: c = Color.Black;
+                    break;
+                case CellState.Green: c = Color.Green;
+                    break;
+                case CellState.Immature: c = Color.Yellow;
+                    break;
+                case CellState.Mature: c = Color.Red;
+                    break;
+                case CellState.Overgrown: c = Color.Brown;
+                    break;
+            }
+            cb.BackColor = c;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeUp_CheckedChanged(object sender, EventArgs e)
+        {
+            timeBack.BackColor = Color.White;
+            timeDown.BackColor = Color.White;
+            timeUp.BackColor = Color.DarkGray;
+            timer1.Interval = 50;          
+
+        }
+
+        private void timeBack_CheckedChanged(object sender, EventArgs e)
+        {
+            timeDown.BackColor = Color.White;
+            timeUp.BackColor = Color.White;
+            timeBack.BackColor = Color.DarkGray;
+            timer1.Interval = 100;
+        }
+
+        private void timeDown_CheckedChanged(object sender, EventArgs e)
+        {
+            timeBack.BackColor = Color.White;
+            timeUp.BackColor = Color.White;
+            timeDown.BackColor = Color.DarkGray;
+            timer1.Interval = 200;            
+        }
+    }
+
+    enum CellState
+    {
+        Empty,
+        Planted,
+        Green,
+        Immature,
+        Mature,
+        Overgrown
+    }
+
+    class Cell
+    {
+        public CellState state = CellState.Empty;
+        public int progress = 0;
+
+        private const int prPlanted = 20;
+        private const int prGreen = 100;
+        private const int prImmature = 120;
+        private const int prMature = 140;
+
+        public void Plant()
+        {
+            state = CellState.Planted;
+            progress = 1;
+        }
+
+        public void Harvest()
+        {
+            state = CellState.Empty;
+            progress = 0;
+        }
+
+        public void NextStep()
+        {
+            if ((state != CellState.Empty) && (state != CellState.Overgrown))
+            {
+                progress++;
+                if (progress < prPlanted) state = CellState.Planted;
+                else if (progress < prGreen) state = CellState.Green;
+                else if (progress < prImmature) state = CellState.Immature;
+                else if (progress < prMature) state = CellState.Mature;
+                else state = CellState.Overgrown;
+            }
+        }
+    }
+
+}
